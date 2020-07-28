381e8183ab1c77f4264ae86d8fe68c5912a92722
naoya0240 <yoshioka0240@gmail.com>
2020-07-28 20:50:28 +0900

    test

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,10 @@
 class Group < ApplicationRecord
+
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
 
   def show_last_message
     if (last_message = messages.last).present?
@@ -16,6 +17,4 @@ class Group < ApplicationRecord
       'まだメッセージはありません。'
     end
   end
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
